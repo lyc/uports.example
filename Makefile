a89d5f4e91cc0647ac6a8c53f5032c4d82e6aeaa
Max Lee <yowching.lee@gmail.com>
2024-08-25 23:02:18 +0800

    Use USE_ALTERNATIVE=yes by default

--- Makefile
+++ Makefile
@@ -19,6 +19,10 @@ FEEDS			?= $(topdir)/feeds
 DESTDIR			?= $(topdir)/local
 PREFIX			?= /usr
 
+PORTS_ENVS		= USE_GLOBALBASE=yes				\
+			  USE_ALTERNATIVE=yes				\
+			  ALTERNATIVE_WRKDIR=$(DESTDIR)/src
+
 # include ports package at here...
 include $(portdir)/Tools/tools.mk
 
---
+++ tests/env-setup.sh
@@ -0,0 +1,15 @@
+export USE_GLOBALBASE=yes
+export DISTDIR_SITE=/opt/distfiles/ports
+export PACKAGES_SITE=/opt/distfiles/ports/packages
+
+#export USE_ALTERNATIVE=yes
+#export ALTERNATIVE_WRKDIR=`pwd`/local/src
+#export FORCE_ALTERNATIVE_REMOVE=yes
+
+#export PORTSDIR=`pwd`/make/uports
+#export DESTDIR=`pwd`/local/
+#export PREFIX=/usr
+
+#export -n PORTSDIR=`pwd`/make/uports
+#export -n DESTDIR=`pwd`/local/
+#export -n PREFIX=/usr
